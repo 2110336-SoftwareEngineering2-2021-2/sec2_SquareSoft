e1f67858bbca941fefc6abe000e911afe5e83294
Chawak <mondcha1507@gmail.com>
2022-01-31 19:22:18 +0700

    INIT : add folder in frontend folder

---
+++ projectlauncher-frontend/src/routes/mainRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+
+const MainRoutes = () => {
+  return (
+    <Switch>
+    </Switch>
+  );
+};
+
+export default MainRoutes;
