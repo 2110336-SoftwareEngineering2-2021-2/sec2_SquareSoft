f2f85cd59f4683946bbd9b50d3e84500ec4acbd1
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-02-13 02:19:27 +0700

    Export RegistrationSystemService from RegistrationSystemModule then import into AuthModule.

--- projectlauncher-backend/src/auth/auth.module.ts
+++ projectlauncher-backend/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { RegistrationSystemModule } from 'src/registration-system/registration-system.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [RegistrationSystemModule],
   providers: [AuthService],
   controllers: [AuthController]
 })
