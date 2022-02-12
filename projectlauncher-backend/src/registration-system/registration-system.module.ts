f815630c50c32dddcfeeba956617483a1c910f46
AK_eNForcer <nonlnw62543@gmail.com>
2022-02-12 21:52:46 +0700

    (DELETE) unused passwdUtil module

--- projectlauncher-backend/src/registration-system/registration-system.module.ts
+++ projectlauncher-backend/src/registration-system/registration-system.module.ts
@@ -3,11 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RegistrationSystemController } from './registration-system.controller';
 import { UserDonatorSchema, UserProjectOwnerSchema } from './registration-system.model';
 import { RegistrationSystemService } from './registration-system.service';
-import { PasswdUtilService } from './passwd-util/passwd-util.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: 'userDonator', schema: UserDonatorSchema}]), MongooseModule.forFeature([{name: 'userProjectOwner', schema: UserProjectOwnerSchema}])],
   controllers: [RegistrationSystemController],
-  providers: [RegistrationSystemService, PasswdUtilService]
+  providers: [RegistrationSystemService]
 })
 export class RegistrationSystemModule {}
