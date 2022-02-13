c52bd48fc426fabf4b49800cddb66ba04c3b1b48
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-02-13 19:56:44 +0700

    (ADD) issue jwt when login successfully.

--- projectlauncher-backend/src/auth/auth.service.ts
+++ projectlauncher-backend/src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
+import { RegistrationSystemService } from 'src/registration-system/registration-system.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private registrationSystemService: RegistrationSystemService,
+        private jwtService: JwtService
+    ) { }
+
+    async login(dto: Object) {
+        const user = await this.registrationSystemService.getUserForLogin(dto["username"]);
+
+        if (!user) {
+            throw new BadRequestException("Incorrect username or password.");
+        } else {
+            const isMatch = await compare(dto["password"], user["hashpassword"]);
+            if (isMatch) {
+                const payload = { username: user["username"] }
+                return this.jwtService.sign(payload);
+            } else {
+                throw new BadRequestException("Incorrect username or password.");
+            }
+        }
+    }
+}
