baa9d50e73f65267cf2b747300ab82c225b305b4
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-02-16 04:26:48 +0700

    (ADD) Create role guard.

---
+++ projectlauncher-backend/src/auth/roles.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { ROLES_KEY } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(
+        private reflector: Reflector,
+        private jwtService: JwtService
+    ) { }
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        if (!requiredRoles || requiredRoles.length <= 0) {
+            return true;
+        }
+
+        const reqeust = context.switchToHttp().getRequest();
+
+        console.log(reqeust.user)
+
+        const token = reqeust.headers.authorization.replace('Bearer ', '');
+        const user = this.jwtService.decode(token);
+        // console.log(user['role'])
+
+        return requiredRoles?.includes(user['role']);
+    }
+}
