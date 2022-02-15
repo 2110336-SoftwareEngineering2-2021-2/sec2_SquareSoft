70f23ca27db54f5fb5690fab01764caec6ebc399
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-02-16 02:17:59 +0700

    (ADD) Import passport strategies.

--- projectlauncher-backend/src/auth/auth.module.ts
+++ projectlauncher-backend/src/auth/auth.module.ts
@@ -4,16 +4,25 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     RegistrationSystemModule,
+    PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '2m' },
-    })
+    }),
   ],
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
+  controllers: [AuthController],
+  exports: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
