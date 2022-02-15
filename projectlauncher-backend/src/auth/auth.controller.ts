504c4b27809e3bb40fff352e0b49ec64e98d8f6a
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-02-16 02:22:14 +0700

    (ADD) Use guard with each endpoint.

--- projectlauncher-backend/src/auth/auth.controller.ts
+++ projectlauncher-backend/src/auth/auth.controller.ts
@@ -1,17 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
+    @UseGuards(LocalAuthGuard)
     @Post('login')
-    async login(@Body() body) {
-        return await this.authService.login(body);
+    async login(@Request() req) {
+        return this.authService.login(req.user);
     }
 
     @Post('refresh-token')
     async refreshToken(@Body() body) {
         return await this.authService.refreshToken(body);
+    }
+
+    // Example usage of JwtAuthGuard
+    // req.user will contain "username" and "role"
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
