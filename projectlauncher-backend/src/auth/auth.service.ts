8f8e7458741c79b45fa97d62c4815904923aade5
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-02-16 17:25:39 +0700

    (EDIT) Add userID to JWT.

--- projectlauncher-backend/src/auth/auth.service.ts
+++ projectlauncher-backend/src/auth/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
     }
 
     async login(user: any) {
-        const payload = { username: user.username, role: user.role };
+        const payload = { _id: user._id, username: user.username, role: user.role };
         return { access_token: this.jwtService.sign(payload) };
     }
 }
