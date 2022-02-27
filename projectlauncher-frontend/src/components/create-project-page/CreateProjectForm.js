bb59f542fba5e4f2cce5a08042c4747ab13f01a5
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-02-27 18:04:53 +0700

    (ADD) Simple component

---
+++ projectlauncher-frontend/src/components/create-project-page/CreateProjectForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class CreateProjectForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="CreateProjectForm">
+        <div>test CreateProjectForm</div>
+      </div>
+    );
+  }
+}
+
+export default CreateProjectForm;
