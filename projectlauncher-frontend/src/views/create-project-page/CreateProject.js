8ce75c46f9147f13ae348a633685a78144356b09
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-02-27 18:37:38 +0700

    (ADD) Redirect user based on role.

--- projectlauncher-frontend/src/views/create-project-page/CreateProject.js
+++ projectlauncher-frontend/src/views/create-project-page/CreateProject.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import CreateProjectForm from "../../components/create-project-page/CreateProjectForm";
 import Navigator from "../../components/navigator";
+import jwt from "jwt-decode";
+import Cookies from "js-cookie";
 
 class CreateProject extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isProjectOwner: false };
+    this.state = {
+      isProjectOwner: !Cookies.get("token")
+        ? false
+        : ["ProjectOwner", "Admin"].includes(jwt(Cookies.get("token")).role),
+    };
   }
 
   render() {
