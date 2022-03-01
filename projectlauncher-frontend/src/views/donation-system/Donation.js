02693ccb4060033940689f7a5e1695f03ef55161
first122 <60084091+first122@users.noreply.github.com>
2022-03-01 15:34:20 +0700

    edit2

---
+++ projectlauncher-frontend/src/views/donation-system/Donation.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import Navigator from "../../components/navigator";
+import TansactionVerification from './admin/transactionVerification'
+import { Flex, Center, Text, Container, VStack } from '@chakra-ui/react'
+import VerificcationBox from '../../../components/donation-system/admin/transaction-verification';
+import { useEffect } from 'react';
+
+
+function Donation(){
+    
+    const [load, setLoad] = useState(false);
+    return(
+        <div>
+            <Navigator />
+            <Container maxW = "container.xl" p = {0}>
+                <Flex px = {20} alignContent = "center">
+                    <Center w='full' py = {5}>
+                        <VStack>
+                            <Text fontSize='3xl' fontWeight="bold" >Transaction Verification</Text>
+                            <VerificcationBox/>
+                            <VerificcationBox/>
+                        </VStack>
+                    </Center>
+                </Flex>
+            </Container>
+        </div>
+    )
+}
+
+export default Donation
