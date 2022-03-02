673d0fb831a9f649eb558003787702e7c84c0c3e
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-03-02 21:14:32 +0700

    (EDIT) Change to functional component.

--- projectlauncher-frontend/src/views/create-project-page/CreateProject.js
+++ projectlauncher-frontend/src/views/create-project-page/CreateProject.js
@@ -1,35 +1,27 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import CreateProjectForm from "../../components/create-project-page/CreateProjectForm";
 import Navigator from "../../components/navigator";
 import jwt from "jwt-decode";
 import Cookies from "js-cookie";
 
-class CreateProject extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isProjectOwner: false };
-  }
+function CreateProject() {
+  const navigate = useNavigate();
 
-  componentDidMount() {
-    if (Cookies.get("token")) { // Check if token exist in Cookies (not null)
-      this.setState({
-        isProjectOwner: ["ProjectOwner", "Admin"].includes(
-          jwt(Cookies.get("token")).role
-        ),
-      });
+  useEffect(() => {
+    if (Cookies.get("token")) {
+      if (!["ProjectOwner", "Admin"].includes(jwt(Cookies.get("token")).role)) {
+        navigate("/");
+      }
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Navigator />
-        <CreateProjectForm />
-        {!this.state.isProjectOwner && <Navigate to="/home" replace={true} />}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Navigator />
+      <CreateProjectForm />
+    </div>
+  );
 }
 
 export default CreateProject;
