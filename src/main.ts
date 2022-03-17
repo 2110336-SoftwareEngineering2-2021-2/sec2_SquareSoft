941d022ac57663107f82d76e31462fe64b9f5176
Watwe <anon.ongsakul2000@gmail.com>
2022-03-17 11:41:22 +0700

    (EDIT) Port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3001;
+  const PORT = process.env.NODE_ENV === "production" ? process.env.PORT : 3001;
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
