7511da60a43621a4adef9eefba3ec3268cf8ce52
AK_eNForcer <nonlnw62543@gmail.com>
2022-03-26 23:46:13 +0700

    (ADD) Edit project

--- src/project/project.dto.ts
+++ src/project/project.dto.ts
@@ -1,4 +1,5 @@
-import { IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
+import { Type } from "class-transformer";
+import { IsDate, IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString, IsUrl, isURL, Max, Min, ValidateNested } from "class-validator";
 
 export class GetProjectDTO{
     @IsNotEmpty()
@@ -13,3 +14,50 @@ export class UpdateProjectDTO extends GetProjectDTO{
     @Max(100)
     progress: number;
 }
+
+export class EditProjectField{
+    @IsOptional()
+    @IsString()
+    projectName: string;
+
+    @IsOptional()
+    @IsString()
+    objective: string;
+
+    @IsOptional()
+    @IsString()
+    description: string;
+
+    @IsOptional()
+    @IsString()
+    fundingType: string;
+
+    @IsOptional()
+    @IsString()
+    category: string;
+    
+    @IsOptional()
+    @IsDateString()
+    deadline: string;
+
+    @IsOptional()
+    @IsNumber()
+    @Min(0.01)
+    fundingGoal: number;
+
+    @IsOptional()
+    @IsString()
+    projectOwnerID: string;
+
+    @IsOptional()
+    @IsUrl()
+    projectPicture: string;
+}
+
+export class EditProjectDTO extends GetProjectDTO{
+    @IsNotEmpty()
+    @Type(() => EditProjectField)
+    @ValidateNested()
+    fields: EditProjectField;
+}
+  
