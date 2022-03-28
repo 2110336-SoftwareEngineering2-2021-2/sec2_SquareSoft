756883beb7f436210285007e4b24f3e33c92a2d8
AK_eNForcer <nonlnw62543@gmail.com>
2022-03-29 02:39:41 +0700

    (ADD) PO withdraw from project

--- src/project/project.model.ts
+++ src/project/project.model.ts
@@ -14,6 +14,7 @@ export const ProjectSchema = new mongoose.Schema({
   projectVideo: { type: String, required: true },
   projectPublishStatus: { type: String, required: true },
   progress: { type: Number, required: true, default: 0 },
+  withdrawnAmount: { type: Number, required: true, default: 0 },
 });
 
 export interface project{
@@ -28,6 +29,7 @@ export interface project{
   projectOwnerID: string;
   projectPicture: string;
   projectPublishStatus: string;
-  progress: Number;
+  progress: number;
+  withdrawnAmount: number;
 }
 
