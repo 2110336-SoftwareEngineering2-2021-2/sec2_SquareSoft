9f23f6945d7788fcecd7e04956da646480f21655
Watwe <anon.ongsakul2000@gmail.com>
2022-03-29 23:58:49 +0700

    (ADD) Get transaction verification API

--- src/api/donaiton-system/admin/transactionVerification.js
+++ src/api/donaiton-system/admin/transactionVerification.js
@@ -1,2 +1,14 @@
 import axios from 'axios'
-import { basedURL, getToken} from '../..';
+import { basedURL, getToken} from '../../index';
+
+async function getTransactionVerification() {
+    const token = getToken();
+    const config = getConfig(token);
+     
+    const response = await axios.get(basedURL.concat('transaction/adminGetUnfinishedUserTX'), config);
+    console.log(response);
+    return await response;
+}
+
+
+export default {getTransactionVerification};
--- src/views/donation-system/admin/transactionVerification.js
+++ src/views/donation-system/admin/transactionVerification.js
@@ -1,26 +1,24 @@
 import { Flex, Center, Text, Container, VStack } from '@chakra-ui/react'
 import VerificcationBox from '../../../components/donation-system/admin/transaction-verification';
 import { useEffect, useState } from 'react';
-import Navigator from "../../../components/navigator";
-import {Row, Col, Card, Button} from "react-bootstrap";
-import axios from 'axios';
-import {basedURL, getToken} from "../../../api/index.js"
+import NavigatorAdmin from "../../../components/navigator-admin";
+import useForAdmin from '../../../components/for-admin';
+import { getTransactionVerification } from './../../../api/donaiton-system/admin/transactionVerification';
 
 function TransactionVerification(){
+    useForAdmin();
+    const [data, setData] = useState(null);
 
-    const [load, setLoad] = useState(false);
-
-    //uncomment to edit loading API
-    /*
     useEffect(()=>{
-        if (!load){
-            getData().then(res => {
+        if (!data){
+            getTransactionVerification()
+            .then(res => {
+                    console.log(res);
                     setData(res.data); 
-                    setLoad(true);
-                }).error();
+            })
+            .catch();
         }
     });
-    */
 
     //uncomment to eding loading state views
     /*if(!load)
@@ -30,49 +28,17 @@ function TransactionVerification(){
             <Text>loading data...</Text>
         </div>
     );*/
-    // useEffect(() => {
-    //     realData();
-    // }, []);
-    const realData = () => {axios.get(basedURL.concat('transaction/adminGetUnfinishedUserTX'), {
-        headers: { Authorization: "Bearer " + getToken() }
-    }).then((result)=>{console.log("Fuck code")})
-    }
-    // console.log(realData)
-    
-    const data =[
-        {
-            "_id": "621b5ae5cfc2c5df4d712363",
-            "timestamp": "2022-02-27T11:05:09.041Z",
-            "username": {
-                "username": "test",
-                "role": "ProjectOwner"
-            },
-            "type": "Deposit",
-            "amount": 321421904902.244,
-            "status": "Pending",
-            "data": {
-                "paymentMethod": "bank transfer",
-                "bank": "kbank",
-                "txRef": null
-            },
-            "__v": 0
-        }
-    ]
-    
-    
-    
-
+        // Do not forget to add key to each verification box
     return(
         <div>
-            <Navigator />
+            <NavigatorAdmin />
             <Container maxW = "container.xl" p = {0}>
                 <Flex px = {20} alignContent = "center">
                     <Center w='full' py = {5}>
                         <VStack>
                             <Text fontSize='3xl' fontWeight="bold" >Transaction Verification</Text>
-                            {Array.from({ length: data.length }).map((_, idx) => (
-                                <VerificcationBox data = {data[idx]}/>
-                            ))}
+                            <VerificcationBox/>
+                            <VerificcationBox/>
                         </VStack>
                     </Center>
                 </Flex>
