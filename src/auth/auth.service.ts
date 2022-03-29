118ea8c4ce6c1cb790663b961aaec97f3b64860a
pan2357 <70005659+pan2357@users.noreply.github.com>
2022-03-29 09:36:03 +0700

    (ADD) Receive passwords in body.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, PreconditionFailedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcryptjs';
 import { RegistrationSystemService } from 'src/registration-system/registration-system.service';
@@ -23,5 +23,29 @@ export class AuthService {
     async login(user: any) {
         const payload = { _id: user._id, username: user.username, role: user.role };
         return { access_token: this.jwtService.sign(payload) };
+    }
+
+    async changePassword(user: any, body: Body){
+        console.log(user._id)
+        console.log(typeof user._id)
+        console.log(user.username)
+        console.log(typeof user.username)
+        console.log(user.role)
+        console.log(typeof user.role)
+        console.log(body["oldPass"])
+        console.log(body["newPass"])
+
+        const oldPass = body["oldPass"];
+        const newPass = body["newPass"];
+
+        if(oldPass && newPass){
+            const userObject = await this.registrationSystemService.findByID(user._id, user.role);
+            if(await compare(oldPass, userObject.hashpassword)) return "password matched!";
+            else throw new PreconditionFailedException("Old password is incorrect.");
+
+            // const ret = this.registrationSystemService.changePassword(user._id, user.username, user.role, oldPass, newPass);
+
+
+        } else throw new BadRequestException("Please provide old password and new password.")
     }
 }
