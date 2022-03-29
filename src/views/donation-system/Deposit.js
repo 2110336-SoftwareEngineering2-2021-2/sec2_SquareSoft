7e6dc590a155745354272c8a00aabeb2d43ece49
first122 <60084091+first122@users.noreply.github.com>
2022-03-29 23:36:54 +0700

    commit

--- src/views/donation-system/Deposit.js
+++ src/views/donation-system/Deposit.js
@@ -4,13 +4,57 @@ import './Donation.css'
 import Cookies from 'js-cookie'
 import './Deposit.css'
 import {Input,InputGroup, InputLeftAddon,Stack,Button}  from '@chakra-ui/react'
-
-function handleConfirm(e){
+import axios from 'axios'
+import {basedURL, getToken} from "../../api/index.js";
+async function handleConfirm(e){
     e.preventDefault();
     let bankName = e.target[0].value
     let bankAccountName = e.target[1].value
     let amount = e.target[2].value
     let slip = e.target[3].files[0]
+    let err = ""
+    let state = true;
+    amount = Number(amount)
+    for(var i=0;i<4;i++)
+        if(e.target[i].value === "") state = false
+    if(!state) err+= "Fill in the missing information.\n"
+    if(state){
+        try{
+
+            let inp_data = await axios.post(basedURL.concat('transaction/newUserDeposit'), 
+                {    "amount": amount,
+                "paymentMethod": "bank transfer",
+                "bank": bankName},
+                {
+                    headers: { Authorization: "Bearer "+getToken() },
+                    // data: {amount,paymentMethod:"bank transfer",bank:bankName}
+                }
+            );
+    
+            let inp_data2 = await axios.patch(basedURL.concat('transaction/updateUserDepositRef'), 
+            {    
+                
+                    "internalTXID": inp_data.data.transaction._id,
+                    "txRef": "https://www.kasikornbank.com/SiteCollectionDocuments/personal/digital-banking/kplus/functions/bill-payment/img/img-08-en.png"
+            },
+            {
+                headers: { Authorization: "Bearer "+getToken() },
+                // data: {amount,paymentMethod:"bank transfer",bank:bankName}
+            }
+        );
+    
+            return inp_data.data;
+        }
+        catch(err){
+            console.log(err)
+        }
+    }else{
+        alert(err)
+    }
+    
+
+
+
 }
 
 class Deposit extends React.Component{
