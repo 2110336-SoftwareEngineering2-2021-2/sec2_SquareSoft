bbdf04106339578725cbeb73091484ee42bf87bc
nonya <nonya579@gmail.com>
2022-04-06 23:00:42 +0700

    (ADD) create delete get review

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,6 +1,8 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from 'src/auth/auth.module';
+import { ProjectSchema } from 'src/project/project.model';
+import { ProjectModule } from 'src/project/project.module';
 import { RegistrationSystemModule } from 'src/registration-system/registration-system.module';
 import { ReviewController } from './review.controller';
 import { ReviewSchema } from './review.model';
@@ -8,7 +10,7 @@ import { ReviewService } from './review.service';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: 'review', schema: ReviewSchema }]), 
+        MongooseModule.forFeature([{ name: 'review', schema: ReviewSchema }, { name: 'project', schema: ProjectSchema }]), 
         forwardRef(() => AuthModule),
         RegistrationSystemModule
     ],
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,23 +1,41 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { project } from 'src/project/project.model';
 import { review } from './review.model';
 
 @Injectable()
 export class ReviewService {
     constructor(
-        @InjectModel('review') private readonly reviewModel: Model<review>
+        @InjectModel('review') private readonly reviewModel: Model<review>,
+        @InjectModel('project') private readonly projectModel: Model<project>
     ) { }
 
-
-    
     async createReview(text: String, star: Number, userID: String, projectID: String) {
+
+        let projectObject;
+
+        try {
+            projectObject = await this.projectModel.findOne({ _id: projectID})
+        } catch (err) {
+            throw new HttpException({ "msg": "project not found" }, HttpStatus.BAD_REQUEST)
+        }
+        
+        if (!projectObject) {
+            throw new HttpException({ "msg": "project not found" }, HttpStatus.BAD_REQUEST)
+        }
+
+        const existingReview = await this.reviewModel.findOne({ projectID: projectID, userID: userID})
+
+        if (existingReview) {
+            throw new HttpException({ "msg": "the user has already reviewed the project" }, HttpStatus.BAD_REQUEST)
+        }
 
         const newReview = {text, star, userID, projectID}
         try {
             const newReviewCreated = new this.reviewModel(newReview);
             const result = await newReviewCreated.save();
-            return newReview;
+            return result;
         }
         catch (err) {
             throw new HttpException({
@@ -27,13 +45,34 @@ export class ReviewService {
         }
     }
     
-    async deleteReview() {
-        const results={hello: 'hello'}
-        return results;
+    async deleteReview(reviewID: String, userID: String) {
+
+        const reviewObject = await this.reviewModel.findOne({ _id: reviewID, userID: userID});
+
+        if (!reviewObject) {
+            throw new HttpException({ "msg": "this review does not belong to this user or this review does not exist" }, HttpStatus.BAD_REQUEST)
+        }
+
+        try {
+            const result = await reviewObject.remove()
+            return result
+        } catch (err) {
+            throw new HttpException({
+                "msg": "review deletion failed: database error",
+                "err": err
+            }, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
     }
 
-    async getReview() {
-        const results={hello: 'hello'}
-        return results;
+    async getReview(projectID: String) {
+        try {
+            const result = await this.reviewModel.find({ projectID: projectID });
+            return result;
+        } catch (err) {
+            throw new HttpException({
+                "msg": "review deletion failed: database error",
+                "err": err
+            }, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
     }
 }
