2bf0a14485ffc26a67ae3af7cf01ba312a12be93
nonya <nonya579@gmail.com>
2022-04-09 19:36:13 +0700

    (ADD) add swagger for review

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,12 +1,30 @@
 import { Body, Controller, Delete, Get, Param, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import * as RoleGuard from "src/auth/jwt-auth.guard"
-import { CreateReviewDTO } from './review.dto';
+import { CreateReviewDTO, CreateReviewReturnDTO, DeleteReviewReturnDTO, GetReviewReturnDTO } from './review.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiNotFoundResponse,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from "@nestjs/swagger"
 
+@ApiTags("review")
+@ApiUnauthorizedResponse({ description: "User is not logged in" })
 @Controller('review')
 export class ReviewController {
     constructor(private reviewService: ReviewService ){}
 
+    @ApiBearerAuth()
+    @ApiBody({ type: CreateReviewDTO })
+    @ApiOkResponse({ description: "Return a review", type: CreateReviewReturnDTO })
+    @ApiBadRequestResponse({description: "invalid body"})
     @UseGuards(RoleGuard.AllRoleGuard)
     @Post('')
     async createReview(@Req() req: any, @Body() body: CreateReviewDTO) {
@@ -14,6 +32,10 @@ export class ReviewController {
         return results;
     }
     
+    @ApiBearerAuth()
+    @ApiParam({ name: "id", type: String, description: "A review's ObjectID" })
+    @ApiOkResponse({ description: "Return a deleted review", type: DeleteReviewReturnDTO })
+    @ApiBadRequestResponse({description: "invalid review id"})
     @UseGuards(RoleGuard.AllRoleGuard)
     @Delete(':id')
     async deleteReview(@Req() req: any, @Param('id') reviewID) {
@@ -21,6 +43,10 @@ export class ReviewController {
         return results;
     }
 
+    @ApiBearerAuth()
+    @ApiQuery({ name: "projectID", type: String, required: true })
+    @ApiOkResponse({ description: "Return a list of reviews", type: GetReviewReturnDTO, isArray: true })
+    @ApiBadRequestResponse({description: "invalid project id"})
     @UseGuards(RoleGuard.GuestAndAllRoleGuard)
     @Get('')
     async getReview(@Req() req: any, @Query() query) {
--- src/review/review.dto.ts
+++ src/review/review.dto.ts
@@ -1,17 +1,74 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsDate, IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString, IsUrl, isURL, Max, Min, ValidateNested } from "class-validator";
 
 export class CreateReviewDTO {
     @IsNotEmpty()
     @IsString()
+    @ApiProperty()
     text: string;
 
     @IsNotEmpty()
     @IsNumber()
     @Min(0)
     @Max(5)
+    @ApiProperty()
     star: number;
 
     @IsNotEmpty()
+    @ApiProperty()
     projectID: string;
 }
+
+export class CreateReviewReturnDTO {
+    @ApiProperty()
+    _id: string;
+
+    @ApiProperty()
+    userID: string;
+
+    @ApiProperty()
+    projectID: string;
+
+    @ApiProperty()
+    star: number;
+
+    @ApiProperty()
+    text: string;
+}
+
+export class DeleteReviewReturnDTO {
+    @ApiProperty()
+    _id: string;
+
+    @ApiProperty()
+    userID: string;
+
+    @ApiProperty()
+    projectID: string;
+
+    @ApiProperty()
+    star: number;
+
+    @ApiProperty()
+    text: string;
+}
   
+export class GetReviewReturnDTO {
+    @ApiProperty()
+    _id: string;
+
+    @ApiProperty()
+    userID: string;
+
+    @ApiProperty()
+    projectID: string;
+
+    @ApiProperty()
+    star: number;
+
+    @ApiProperty()
+    text: string;
+
+    @ApiProperty()
+    isOwner: boolean;
+}
